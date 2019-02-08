6ca12007e8893af062981f9e70d731fc39213364
Kentaro Hayashi <kenhys@gmail.com>
2019-02-08 15:38:48 +0900

    Quote

--- langja.ps1
+++ langja.ps1
@@ -1,5 +1,5 @@
 $LpURL = "http://fg.v4.download.windowsupdate.com/d/msdownload/update/software/updt/2013/09"
 $CabFile = "lp_3d6c75e45f3247f9f94721ea8fa1283392d36ea2.cab"
-$AppTemp = Join-Path $Env:LOCALAPPDATA "Temp"
-(New-Object Net.Webclient).DownloadFile("$LpURL/$CabFile", (Join-Path $AppTemp $CabFile))
-Add-WindowsPackage -Online -PackagePath (Join-Path $AppTemp $CabFile)
+$AppTemp = (Join-Path $Env:LOCALAPPDATA "Temp")
+(New-Object Net.Webclient).DownloadFile("$LpURL/$CabFile", (Join-Path "$AppTemp" "$CabFile"))
+Add-WindowsPackage -Online -PackagePath (Join-Path "$AppTemp" "$CabFile")
