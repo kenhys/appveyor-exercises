6c738dfa808a38763e3a9e148796c8497270901e
Kentaro Hayashi <kenhys@gmail.com>
2019-02-14 23:29:01 +0900

    Fix

--- command_runner.rb
+++ command_runner.rb
@@ -126,6 +126,7 @@ module CommandRunner
 
       candidates.each do |candidate_program_path|
         return candidate_program_path if File.exist?(candidate_program_path)
+        p "check... #{candidate_program_path}" if candidate_program_path.begin_with?("C:\\projects")
       end
     end
     p "CAN'T FIND #{name}!!!"
