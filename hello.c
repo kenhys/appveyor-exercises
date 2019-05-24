a8582c33a472e4511a0368d3a1f9d054eebd9e0e
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-24 14:26:49 +0900

    Try to use wmemmove

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <wchar.t>
 
 int main(int argc, char *argv[]){
-	return 0;
+  wchar_t name[] = L"\\\\?\\\C:\projects\nroonga";
+  size_t length = wcslen(name);
+  wmemmove(name, name+4, length);
+  name[length - 4] L'\\0';
+  printf("%ls\n", name);
+  return 0;
 }
