cb686672427c417a169c5ab9514093090e19fdfa
Kentaro Hayashi <hayashi@clear-code.com>
2019-08-28 18:44:29 +0900

    debug install-mroonga.ps1

---
+++ install-mroonga.ps1
@@ -0,0 +1,38 @@
+Param(
+  [Parameter(mandatory=$true)][String]$mariadbVersion,
+  [Parameter(mandatory=$true)][String]$platform
+)
+
+function Wait-UntilRunning($cmdName) {
+  do
+  {
+    Start-Sleep -s 5
+    $Running = Get-Process $cmdName -ErrorAction SilentlyContinue
+    Write-Output "Wait-UntilRunning"
+    Write-Output $Running
+  } while (!$Running)
+}
+
+function Wait-UntilTerminate($cmdName) {
+  do
+  {
+    Start-Sleep -s 5
+    $Running = Get-Process $cmdName -ErrorAction SilentlyContinue
+    Write-Output "Wait-UntilTerminate"
+    Write-Output $Running
+  } while ($Running)
+}
+
+function Install-Mroonga($mariadbVer, $arch, $installSqlDir) {
+  cd "mariadb-$mariadbVer-$arch"
+  Start-Process .\bin\mysqld.exe
+  Wait-UntilRunning mysqld
+  Get-Content "$installSqlDir\install.sql" | .\bin\mysql.exe -uroot
+  Start-Process .\bin\mysqladmin.exe -ArgumentList "-uroot shutdown"
+  Wait-UntilTerminate mysqld
+  cd ..
+}
+
+$installSqlDir = ".\share\mroonga"
+
+Install-Mroonga $mariadbVersion $platform $installSqlDir
