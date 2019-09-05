91993837568a0d0f78a6bbe479e35e736176bd79
Kentaro Hayashi <hayashi@clear-code.com>
2019-09-05 11:40:47 +0900

    Use Run-MySQL, Run-MySQLInstallDB, Run-MySQLAdmin function

--- install-mroonga.ps1
+++ install-mroonga.ps1
@@ -3,50 +3,51 @@ Param(
   [Parameter(mandatory=$true)][String]$platform
 )
 
-function Wait-UntilRunning($cmdName) {
+function Run-MySQL {
+  Write-Output("Start mysqld.exe")
+  Start-Process .\bin\mysqld.exe -ArgumentList "--console" -RedirectStandardError ..\mysqld.txt
   $Waiting = $TRUE
   do
   {
-    switch ($cmdName) {
-      "mysqld" {
-        if (Test-Path ..\mysqld.txt) {
-	  $Version = Get-Content ..\mysqld.txt | Select-String -Pattern "^Version:"
-	  if ($Version) {
-	    $Waiting = $FALSE
-	  } else {
-	    Write-Output $Version
-	  }
-        }
+    if (Test-Path ..\mysqld.txt) {
+      $Version = Get-Content ..\mysqld.txt | Select-String -Pattern "^Version:"
+      if ($Version) {
+        $Waiting = $FALSE
       }
-      "mysql_install_db" {
-        if (Test-Path ..\install.txt) {
-	  $Successful = Get-Content ..\install.txt | Select-String -Pattern "successful"
-	  if ($Successful) {
-	    $Waiting = $FALSE
-	  } else {
- 	    Write-Output $Successful
-	  }
-        }
+    }
+  } while ($Waiting)
+}
+
+function Run-MySQLInstallDB {
+  Write-Output("Start mysql_install_db.exe")
+  Start-Process .\bin\mysql_install_db.exe -RedirectStandardOutput ..\install.txt
+  $Waiting = $TRUE
+  do
+  { 
+    if (Test-Path ..\install.txt) {
+      $Successful = Get-Content ..\install.txt | Select-String -Pattern "successful"
+      if ($Successful) {
+        $Waiting = $FALSE
       }
     }
     Start-Sleep -s 1
   } while ($Waiting)
 }
 
-function Wait-UntilTerminate($cmdName) {
-  $Running = $TRUE
+function Run-MySQLAdmin {
+  Write-Output("Shutdown mysqld.exe")
+  Start-Process .\bin\mysqladmin.exe -ArgumentList "-uroot shutdown"
+  $Waiting = $TRUE
   do
   {
     if (Test-Path ..\mysqld.txt) {
       $Complete = Get-Content ..\mysqld.txt | Select-String -Pattern "Shutdown complete"
       if ($Complete) {
-        $Running = $FALSE
-      } else {
-        Write-Output $Complete
+        $Waiting = $FALSE
       }
     }
     Start-Sleep -s 1
-  } while ($Running)
+  } while ($Waiting)
 }
 
 function Install-Mroonga($mariadbVer, $arch, $installSqlDir) {
@@ -55,17 +56,12 @@ function Install-Mroonga($mariadbVer, $arch, $installSqlDir) {
   if ("$mariadbVer" -eq "10.4.7") {
     Write-Output("Clean data directory")
     Remove-Item data -Recurse
-    Start-Process .\bin\mysql_install_db.exe -RedirectStandardOutput ..\install.txt
-    Wait-UntilRunning mysql_install_db
+    Run-MySQLInstallDB
   }
-  Write-Output("Start mysqld.exe")
-  Start-Process .\bin\mysqld.exe -ArgumentList "--console" -RedirectStandardError ..\mysqld.txt
-  Wait-UntilRunning mysqld
+  Run-MySQL
   Write-Output("Execute install.sql")
   Get-Content "$installSqlDir\install.sql" | .\bin\mysql.exe -uroot
-  Write-Output("Shutdown mysqld.exe")
-  Start-Process .\bin\mysqladmin.exe -ArgumentList "-uroot shutdown"
-  Wait-UntilTerminate mysqld
+  Run-MySQLAdmin
   cd ..
   Write-Output("Finished to install Mroonga")
 }
